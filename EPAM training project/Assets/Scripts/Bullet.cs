5397eff20f97df27c5800c966546527a60e26a76
Николай Кожевников <nkozhevn2000@gmail.com>
2021-09-15 21:47:36 +0300

    Problems fixed

--- EPAM training project/Assets/Scripts/Bullet.cs
+++ EPAM training project/Assets/Scripts/Bullet.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
+    [SerializeField]
+    private GameObject hitEffect;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.tag == "Player")
+        {
+            return;
+        }
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(effect, 0.25f);
         Destroy(gameObject);
--- EPAM training project/Assets/Scripts/CameraFollow.cs
+++ EPAM training project/Assets/Scripts/CameraFollow.cs
@@ -9,7 +9,7 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.25f;
     public Vector3 offset;
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
